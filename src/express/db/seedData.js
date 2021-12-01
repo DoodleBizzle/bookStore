c2113cb3d34bd1cad40c3968b652e65a43ff6800
Sordot <tgevirtz@gmail.com>
2021-12-01 13:58:53 -0600

    creates products table and writes INSERT function to seed db

--- src/express/db/seedData.js
+++ src/express/db/seedData.js
@@ -23,7 +23,20 @@ async function rebuildDB() {
         "userID" INTEGER REFERENCES users(id),
         "productID" INTEGER REFERENCES products(id),
         quantity INTEGER
-      );  
+      );
+      
+      CREATE TABLE products(
+        id SERIAL PRIMARY KEY,
+        title VARCHAR(255) NOT NULL,
+        description TEXT,
+        author VARCHAR(255) NOT NULL,
+        format VARCHAR(255) NOT NULL,
+        "genreID" INTEGER REFERENCES genres(id),
+        isbn INTEGER UNIQUE NOT NULL,
+        cover_url TEXT,
+        price FLOAT(6,2) NOT NULL,
+        stock INTEGER NOT NULL
+      );
     `)    // drop tables in correct order
     // build tables in correct order
   } catch (error) {
@@ -38,12 +51,24 @@ async function seedData() {
     { email: "testuser", password: "testuser999" }
   ];
 
+  const products = [
+    {title: "Notes from Underground", author: "Fyodor Dostoevsky", description: "A good book.", format: "Paperback", isbn: 9780679734529, cover_url: "www.google.com", price: 12.99, stock: 2}
+  ]
+
   for(const user of users) {
     await client.query(/*sql*/`
       INSERT INTO users
       (email, password)
       VALUES ($1, $2);
     `,[user.email, user.password]);
+  }
+
+  for(const product of products) {
+    await client.query(`
+      INSERT INTO products
+      (title, description, author, format, isbn, cover_url, price, stock)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8);
+    `, [product.title, product.description, product.author, , product.format, product.isbn, product.cover_url, product.price, product.stock]);
   }
 
   // create useful starting data
