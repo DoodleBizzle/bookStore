506377881657815114a2cf313c18e9d5b2b5354c
Sordot <tgevirtz@gmail.com>
2021-12-03 11:36:25 -0600

    adds more GET routes

--- src/express/db/productsMethods.js
+++ src/express/db/productsMethods.js
@@ -12,6 +12,18 @@ async function getAllProducts() {
     }
 }
 
+async function getProductByID(productID) {
+    try {
+        const {rows: [product]} = await client.query(`
+        SELECT *
+        FROM products
+        WHERE id = ($1);
+    `, [productID])
+    return product
+    } catch (error) {
+        console.error(error)
+    }
+}
 
 async function getProductByID(productID) {
     try {
@@ -26,7 +38,77 @@ async function getProductByID(productID) {
     }
 }
 
+async function getProductByTitle(productTitle) {
+    try {
+        const {rows: [product]} = await client.query(`
+        SELECT *
+        FROM products
+        WHERE title = ($1);
+    `, [productTitle])
+    return product
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+async function getProductByAuthor(productAuthor) {
+    try {
+        const {rows: [product]} = await client.query(`
+        SELECT *
+        FROM products
+        WHERE author = ($1);
+    `, [productAuthor])
+    return product
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+async function getProductByFormat(productFormat) {
+    try {
+        const {rows: [product]} = await client.query(`
+        SELECT *
+        FROM products
+        WHERE format = ($1);
+    `, [productFormat])
+    return product
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+async function getProductByISBN(productISBN) {
+    try {
+        const {rows: [product]} = await client.query(`
+        SELECT *
+        FROM products
+        WHERE isbn = ($1);
+    `, [productISBN])
+    return product
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+async function getProductByGenre(genreID) {
+    try {
+        const {rows: [product]} = await client.query(`
+        SELECT *
+        FROM products
+        WHERE "genreID" = ($1);
+    `, [genreID])
+    return product
+    } catch (error) {
+        console.error(error)
+    }
+}
+
 module.exports = {
     getAllProducts,
-    getProductByID
+    getProductByID,
+    getProductByTitle,
+    getProductByAuthor,
+    getProductByFormat,
+    getProductByISBN,
+    getProductByGenre
 }
--- src/express/routes/productsRouter.js
+++ src/express/routes/productsRouter.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const productsRouter = express.Router();
 const {requireUser} = require('./utils');
-const {getAllProducts} = require('../db/productsMethods')
+const {getAllProducts, 
+        getProductByID, 
+        getProductByTitle, 
+        getProductByAuthor, 
+        getProductByFormat, 
+        getProductByISBN,
+        getProductByGenre} = require('../db/productsMethods')
 
 productsRouter.use((req, res, next) => {
     console.log("A request is being made to /users");
@@ -33,8 +39,70 @@ productsRouter.get('/:productID', async (req, res) => {
     }
 });
 
+productsRouter.get('/:title', async (req, res) => {
 
+    try {
+        const {title} = req.params;
+        const product = await getProductByTitle(title)
+        res.send(
+            product
+        );
+    } catch (error) {
+        console.error(error)
+    }
+});
 
+productsRouter.get('/:author', async (req, res) => {
+
+    try {
+        const {author} = req.params;
+        const product = await getProductByAuthor(author)
+        res.send(
+            product
+        );
+    } catch (error) {
+        console.error(error)
+    }
+});
+
+productsRouter.get('/:format', async (req, res) => {
+
+    try {
+        const {format} = req.params;
+        const product = await getProductByFormat(format)
+        res.send(
+            product
+        );
+    } catch (error) {
+        console.error(error)
+    }
+});
+
+productsRouter.get('/:isbn', async (req, res) => {
+
+    try {
+        const {isbn} = req.params;
+        const product = await getProductByISBN(isbn)
+        res.send(
+            product
+        );
+    } catch (error) {
+        console.error(error)
+    }
+});
+
+productsRouter.get('/:genreID', async (req, res) => {
+
+    try {
+        const {genreID} = req.params;
+        const product = await getProductByGenre(genreID)
+        res.send(
+            product
+        );
+    } catch (error) {
+        console.error(error)
+    }
+});
 
 
 
